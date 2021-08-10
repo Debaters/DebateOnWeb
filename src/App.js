aa7d02c739c2c877141998ce8ebae70b06878325
Kim-Myoung-jae <rlaaud30@naver.com>
2021-08-10 23:53:04 +0900

    init commit done :)

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Hello Start Side Project With React.js
         </a>
       </header>
     </div>
