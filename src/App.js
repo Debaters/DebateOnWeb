faecc120fff680cb1b4dfed70f3dcd1ed149b4bf
CASY82 <repaid11010@gmail.com>
2021-08-20 04:03:42 +0900

    Debate_Room 작성중, 아직 미완성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { render } from '@testing-library/react';
+import './Debate_Room/Debate_Room';
+import Debate_Room from './Debate_Room/Debate_Room';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Debate_Room></Debate_Room>
+      </div>
+    )
+
+  }
 }
 
 export default App;
