5da2534398a20d88a7df440b894368894898407e
wlals822 <wlals822@naver.com>
2021-08-31 21:56:36 +0900

    Cors 수정 공유용 커밋 (삭제 예정)

--- src/index.js
+++ src/index.js
@@ -11,6 +11,21 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+async function getTitles() {
+  const response = await fetch("/graphql", {
+    body: `{"query":"{titles}"}`,
+    headers: {
+      Accept: "application/json",
+      "Api-Key": "demoKeyOfApi",
+      "Content-Type": "application/json",
+      Dnt: "1"
+    },
+    method: "POST"
+  })
+  console.log(response.json());
+};
+
+getTitles();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
